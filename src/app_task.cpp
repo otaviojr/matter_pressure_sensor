dbdf92b121f3a4e929f3a1004f87f53f5feecc34
Otávio Ribeiro <otavio.ribeiro@gmail.com>
2023-09-11 23:20:49 -0300

    Configuring ADC

--- src/app_task.cpp
+++ src/app_task.cpp
@@ -29,6 +29,9 @@
 #endif
 
 #include <dk_buttons_and_leds.h>
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/drivers/adc.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
@@ -62,26 +65,39 @@ bool sHaveBLEConnections = false;
 
 namespace LedConsts
 {
-namespace StatusLed
-{
-	namespace Unprovisioned
+	namespace StatusLed
 	{
-		constexpr uint32_t kOn_ms{ 100 };
-		constexpr uint32_t kOff_ms{ kOn_ms };
-	} /* namespace Unprovisioned */
-	namespace Provisioned
-	{
-		constexpr uint32_t kOn_ms{ 50 };
-		constexpr uint32_t kOff_ms{ 950 };
-	} /* namespace Provisioned */
+		namespace Unprovisioned
+		{
+			constexpr uint32_t kOn_ms{ 100 };
+			constexpr uint32_t kOff_ms{ kOn_ms };
+		} /* namespace Unprovisioned */
+		namespace Provisioned
+		{
+			constexpr uint32_t kOn_ms{ 50 };
+			constexpr uint32_t kOff_ms{ 950 };
+		} /* namespace Provisioned */
 
-} /* namespace StatusLed */
+	} /* namespace StatusLed */
 } /* namespace LedConsts */
 
 #ifdef CONFIG_CHIP_WIFI
 app::Clusters::NetworkCommissioning::Instance
 	sWiFiCommissioningInstance(0, &(NetworkCommissioning::NrfWiFiDriver::Instance()));
 #endif
+
+#if !DT_NODE_EXISTS(DT_PATH(zephyr_user)) || \
+	!DT_NODE_HAS_PROP(DT_PATH(zephyr_user), io_channels)
+#error "No suitable devicetree overlay specified. ADC io-channel missing!"
+#endif
+
+#define DT_SPEC_AND_COMMA(node_id, prop, idx) \
+	ADC_DT_SPEC_GET_BY_IDX(node_id, idx),
+
+static const struct adc_dt_spec adc_channels[] = {
+	DT_FOREACH_PROP_ELEM(DT_PATH(zephyr_user), io_channels,
+			     DT_SPEC_AND_COMMA)
+};
 
 CHIP_ERROR AppTask::Init()
 {
@@ -182,11 +198,65 @@ CHIP_ERROR AppTask::StartApp()
 {
 	ReturnErrorOnFailure(Init());
 
+	/* Configure ADC channels individually prior to sampling. */
+	for (size_t i = 0U; i < ARRAY_SIZE(adc_channels); i++) {
+		if (!device_is_ready(adc_channels[i].dev)) {
+			LOG_ERR("ADC controller device not ready\n");
+			break;
+		}
+
+		int err = adc_channel_setup_dt(&adc_channels[i]);
+		if (err < 0) {
+			LOG_ERR("Could not setup channel #%d (%d)\n", i, err);
+			break;
+		}
+	}
+
 	AppEvent event = {};
 
 	while (true) {
 		k_msgq_get(&sAppEventQueue, &event, K_FOREVER);
 		DispatchEvent(event);
+
+		/*int16_t buf;
+		struct adc_sequence sequence = {
+			.buffer = &buf,
+			// buffer size in bytes, not number of samples
+			.buffer_size = sizeof(buf),
+		};*/
+
+		/*while (1) {
+			printk("ADC reading:\n");
+			for (size_t i = 0U; i < ARRAY_SIZE(adc_channels); i++) {
+				int32_t val_mv;
+
+				printk("- %s, channel %d: ",
+					adc_channels[i].dev->name,
+					adc_channels[i].channel_id);
+
+				(void)adc_sequence_init_dt(&adc_channels[i], &sequence);
+
+				err = adc_read(adc_channels[i].dev, &sequence);
+				if (err < 0) {
+					printk("Could not read (%d)\n", err);
+					continue;
+				} else {
+					printk("%"PRId16, buf);
+				}
+
+				// conversion to mV may not be supported, skip if not
+				val_mv = buf;
+				err = adc_raw_to_millivolts_dt(&adc_channels[i],
+								&val_mv);
+				if (err < 0) {
+					printk(" (value in mV not available)\n");
+				} else {
+					printk(" = %"PRId32" mV\n", val_mv);
+				}
+			}
+
+			k_sleep(K_MSEC(1000));
+		}*/
 	}
 
 	return CHIP_NO_ERROR;
